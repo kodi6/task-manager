4e34da4800c513b026bc9a9784dd76a3ad3da6a5
kodi6 <punniyakodi.office@gmail.com>
2024-09-03 23:28:37 +0530

    Add test for get_user_task/2

--- test/task_manager/tasks_test.exs
+++ test/task_manager/tasks_test.exs
@@ -31,8 +31,21 @@ defmodule TaskManager.TasksTest do
     test "get_user_tasks/1 returns all tasks with given user id" do
       user = user_fixture()
       task = task_fixture(%{user_id: user.id})
-      IO.inspect(task, label: "task")
       assert Tasks.get_user_tasks(user.id) == [task]
+    end
+
+    test "get_user_task/2 retrieves a specific task for the specified user" do
+      user = user_fixture()
+      task = task_fixture(%{user_id: user.id})
+      fetched_task = Tasks.get_user_task(user.id, task.id)
+
+      IO.inspect(fetched_task, label: "fetched_task")
+      assert fetched_task.id == task.id
+      assert fetched_task.status == task.status
+      assert fetched_task.title == task.title
+      assert fetched_task.description == task.description
+      assert fetched_task.due_date == task.due_date
+      assert fetched_task.user_id == task.user_id
     end
   end
 end
