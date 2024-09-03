7adf9c10ca44ee57f987dac9b032c33807a4c785
kodi6 <punniyakodi.office@gmail.com>
2024-09-03 20:33:19 +0530

    Add task_fixture helper for creating test tasks

---
+++ test/support/fixtures/tasks_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule TaskManager.TasksFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `TaskManager.Tasks` context.
+  """
+
+  @doc """
+  Generate a task.
+  """
+  def task_fixture(attrs \\ %{}) do
+    {:ok, task} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        due_date: ~D[2024-09-15],
+        status: "some status",
+        title: "some title"
+      })
+      |> TaskManager.Tasks.create_task()
+    task
+  end
+end
